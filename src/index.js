34429ac3cf541d43377279415d1bbf0f6952704c
lisamoser <lisamoser1@hotmail.com>
2024-03-24 14:45:16 +1100

    Added API search function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let searchCityElement = document.querySelector("#weather-app-city");
+
+  searchCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "7odf01a4dd3067tb31ab818f7aa1b23f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function runCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city-input");
-  let searchCityElement = document.querySelector("#weather-app-city");
-  searchCityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", runCitySearch);
+
+searchCity("Melbourne");
