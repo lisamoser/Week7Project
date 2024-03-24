99ad6094dd875ef2d858a2dc9c661af6079c7b75
lisamoser <lisamoser1@hotmail.com>
2024-03-24 15:39:07 +1100

    Used API for current weather and time data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,37 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let searchCityElement = document.querySelector("#weather-app-city");
+  let timeElement = document.querySelector("#time");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let date = new Date(response.data.time * 1000);
 
   searchCityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = returnDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
-
+function returnDate(date) {
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 0) {
+    `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "7odf01a4dd3067tb31ab818f7aa1b23f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
